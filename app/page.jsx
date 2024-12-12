67ec0f049c754e54a5c3f4054ed3c65f3897f313
RjCoder <ej84@njit.edu>
2024-12-12 16:01:26 -0500

    Debugging front page.

--- app/page.jsx
+++ app/page.jsx
@@ -16,50 +16,54 @@ export default function Home() {
   return (
     <>
       <Nav />
-      <main className="flex min-h-screen flex-col items-center justify-normal p-32">
+      <main className="flex min-h-screen flex-col items-center justify-normal p-28">
         <div className="container mx-auto">
           <div className="flex justify-center">
             <h1 className="text-4xl font-bold mb-4">All You Can Convert</h1>
           </div>
 
-          <div className="grid grid-cols-2 justify-center p-32">
-            <Link href="/convert-image" className="btn1">
-              <div className="flex items-center">
-                <span className="pr-5">
-                  <FontAwesomeIcon icon={faFileImage} size="3x" />
-                </span>
-                <div>
-                  <p className="text-xl text-left">Image Converter</p>
-                  <p className="text-base">
-                    Convert your image file to any format you want.
-                  </p>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 md:gap-10 p-10">
+            <div className="btn1">
+              <Link href="/convert-image">
+                <div className="flex items-center">
+                  <span className="pr-5">
+                    <FontAwesomeIcon icon={faFileImage} size="3x" />
+                  </span>
+                  <div>
+                    <p className="text-xl text-left">Image Converter</p>
+                    <p className="text-base">
+                      Convert your image file to any format you want.
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </Link>
-
-            <Link href="/convert-video" className="btn1 ml-5">
-              <div className="flex items-center">
-                <span className="pr-5">
-                  <FontAwesomeIcon icon={faFileVideo} size="3x" />
-                </span>
-                <div>
-                  <p className="text-xl text-left">Convert Video</p>
-                  <p className="text-base">(MP4, AVI, WAV and More)</p>
+              </Link>
+            </div>
+            <div className="btn1">
+              <Link href="/convert-video">
+                <div className="flex items-center">
+                  <span className="pr-5">
+                    <FontAwesomeIcon icon={faFileVideo} size="3x" />
+                  </span>
+                  <div>
+                    <p className="text-xl text-left">Convert Video</p>
+                    <p className="text-base">(MP4, AVI, WAV and More)</p>
+                  </div>
                 </div>
-              </div>
-            </Link>
-
-            <Link href="/convert-document" className="btn1 mt-5">
-              <div className="flex items-center">
-                <span className="pr-5">
-                  <FontAwesomeIcon icon={faFilePdf} size="3x" />
-                </span>
-                <div>
-                  <p className="text-xl text-left">Convert Document</p>
-                  <p className="text-base">(PDF, DOCX, CSV and More)</p>
+              </Link>
+            </div>
+            <div className="btn1">
+              <Link href="/convert-document">
+                <div className="flex items-center">
+                  <span className="pr-5">
+                    <FontAwesomeIcon icon={faFilePdf} size="3x" />
+                  </span>
+                  <div>
+                    <p className="text-xl text-left">Convert Document</p>
+                    <p className="text-base">(PDF, DOCX, CSV and More)</p>
+                  </div>
                 </div>
-              </div>
-            </Link>
+              </Link>
+            </div>
           </div>
           {/*<div className="flex justify-center">
           <div className="grid grid-cols-3 text-center">
